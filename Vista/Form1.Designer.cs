ca39789e802402266939bb542ed0c4616821fefa
Jose Miguel Upia <upia915@gmail.com>
2021-03-13 00:00:55 -0400

    Elementos de prueba

--- Vista/Form1.Designer.cs
+++ Vista/Form1.Designer.cs
@@ -30,6 +30,7 @@ namespace Sistema_de_gestion_de_productos
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -37,16 +38,26 @@ namespace Sistema_de_gestion_de_productos
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(271, 170);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.Size = new System.Drawing.Size(91, 15);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Label de prueba";
             this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(289, 205);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 48);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Boton de prueba";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -58,6 +69,7 @@ namespace Sistema_de_gestion_de_productos
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
