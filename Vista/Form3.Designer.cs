528ee7012689e0eff606a3dd2f14bbd62d75cdf8
Kelvin Hernandez <youmater2001@gmail.com>
2021-03-18 16:30:46 -0400

    Cambios sencillos al form 3; añadí labels y el botón buscar.

--- Vista/Form3.Designer.cs
+++ Vista/Form3.Designer.cs
@@ -37,12 +37,22 @@ namespace Sistema_de_gestion_de_productos.Vista
             this.button2 = new System.Windows.Forms.Button();
             this.bnAgregar = new System.Windows.Forms.Button();
             this.dataGridCategotia = new System.Windows.Forms.DataGridView();
-            this.label8 = new System.Windows.Forms.Label();           
-            this.txtID = new System.Windows.Forms.TextBox();            
-            this.txtNombre = new System.Windows.Forms.TextBox();           
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
             this.cbEstado = new System.Windows.Forms.ComboBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnBuscarCat = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridCategotia)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
@@ -129,11 +139,9 @@ namespace Sistema_de_gestion_de_productos.Vista
             dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(69)))), ((int)(((byte)(120)))));
             dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.White;
             this.dataGridCategotia.RowsDefaultCellStyle = dataGridViewCellStyle2;
-            this.dataGridCategotia.RowTemplate.Height = 25;
             this.dataGridCategotia.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridCategotia.Size = new System.Drawing.Size(665, 329);
             this.dataGridCategotia.TabIndex = 11;
-            
             // 
             // label8
             // 
@@ -143,8 +151,6 @@ namespace Sistema_de_gestion_de_productos.Vista
             this.label8.Size = new System.Drawing.Size(36, 15);
             this.label8.TabIndex = 10;
             this.label8.Text = "Stock";
-                                  
-            
             // 
             // txtID
             // 
@@ -154,7 +160,6 @@ namespace Sistema_de_gestion_de_productos.Vista
             this.txtID.Name = "txtID";
             this.txtID.Size = new System.Drawing.Size(70, 23);
             this.txtID.TabIndex = 1;
-            
             // 
             // txtNombre
             // 
@@ -164,19 +169,27 @@ namespace Sistema_de_gestion_de_productos.Vista
             this.txtNombre.Name = "txtNombre";
             this.txtNombre.Size = new System.Drawing.Size(134, 23);
             this.txtNombre.TabIndex = 9;
-            
+            // 
             // cbEstado
-            // comboBox2
+            // 
             this.cbEstado.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.cbEstado.FormattingEnabled = true;
-            this.cbEstado.Items.AddRange(new object[] {});
+            this.cbEstado.Items.AddRange(new object[] {
+            "Activo",
+            "Inactivo"});
             this.cbEstado.Location = new System.Drawing.Point(507, 32);
             this.cbEstado.Name = "cbEstado";
             this.cbEstado.Size = new System.Drawing.Size(134, 23);
-            this.cbEstado.TabIndex = 18;            
-            this.groupBox1.Controls.Add(this.cbEstado);          
-            this.groupBox1.Controls.Add(this.txtNombre);            
-            this.groupBox1.Controls.Add(this.txtID);            
+            this.cbEstado.TabIndex = 18;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cbEstado);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Controls.Add(this.txtID);
             this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.groupBox1.ForeColor = System.Drawing.Color.White;
             this.groupBox1.Location = new System.Drawing.Point(12, 24);
@@ -184,13 +197,14 @@ namespace Sistema_de_gestion_de_productos.Vista
             this.groupBox1.Size = new System.Drawing.Size(691, 78);
             this.groupBox1.TabIndex = 12;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Agregar Categoría";
+            this.groupBox1.Text = "Estado";
             // 
             // groupBox2
             // 
             this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.btnBuscarCat);
             this.groupBox2.Controls.Add(this.label2);
             this.groupBox2.Controls.Add(this.button3);
             this.groupBox2.Controls.Add(this.textBox1);
@@ -205,6 +219,99 @@ namespace Sistema_de_gestion_de_productos.Vista
             this.groupBox2.TabIndex = 19;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Agregar Categoría";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 15);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(206, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 15);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Nombre";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(206, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 15);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Nombre";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(37, 35);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(18, 15);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "ID";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(507, 32);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(134, 23);
+            this.comboBox1.TabIndex = 18;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(263, 32);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(134, 23);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(61, 32);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(70, 23);
+            this.textBox3.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(206, 35);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 15);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Nombre";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(450, 35);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(42, 15);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Estado";
+            // 
+            // btnBuscarCat
+            // 
+            this.btnBuscarCat.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBuscarCat.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(37)))), ((int)(((byte)(69)))));
+            this.btnBuscarCat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBuscarCat.Location = new System.Drawing.Point(304, 26);
+            this.btnBuscarCat.Name = "btnBuscarCat";
+            this.btnBuscarCat.Size = new System.Drawing.Size(75, 27);
+            this.btnBuscarCat.TabIndex = 13;
+            this.btnBuscarCat.Text = "Buscar";
+            this.btnBuscarCat.UseVisualStyleBackColor = true;
             // 
             // Form3
             // 
@@ -242,5 +349,15 @@ namespace Sistema_de_gestion_de_productos.Vista
         public System.Windows.Forms.TextBox txtNombre;
         public System.Windows.Forms.ComboBox cbEstado;
         private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnBuscarCat;
     }
 }
