f4faf50828428013500d77c1fc59da783c32831e
numbasan-san <eddymanuel20@gmail.com>
2021-03-19 17:58:46 -0400

    Establecimiento de los métodos buscar, eliminar, modificar; completado del procedimiento del método CtlProducto

--- Control/CtlProducto.cs
+++ Control/CtlProducto.cs
@@ -11,6 +11,7 @@ namespace Sistema_de_gestion_de_productos.Control
     class CtlProducto
     {
         private ProductoDao producto;
+        Producto contenedor;
         private Form2 vistaPro;        
 
         public CtlProducto(Form2 vistaPro)
@@ -18,14 +19,18 @@ namespace Sistema_de_gestion_de_productos.Control
             this.vistaPro = vistaPro;
             vistaPro.Load += new EventHandler(cargarDatos);
             vistaPro.btAgregar.Click += new EventHandler(agregar);
+            vistaPro.btnBuscarProduct.Click += new EventHandler(buscar);
+            vistaPro.btEliminar.Click += new EventHandler(eliminar);
+            vistaPro.btModificar.Click += new EventHandler(modificar);
 
-            producto = new ProductoDao();            
+            producto = new ProductoDao();
+            contenedor = new Producto();    
         }
 
         private void cargarDatos(object sender, EventArgs e)
         {
             CategoriaDao categoria = new CategoriaDao();
-            vistaPro.dataGridProductor.DataSource = producto.verRegistro();                      
+            vistaPro.dataGridProducto.DataSource = producto.verRegistro();         
             foreach (Categoria c in categoria.verRegistro())
             {
                 vistaPro.cbCategoria.Items.Add( c.Id);                
@@ -35,34 +40,44 @@ namespace Sistema_de_gestion_de_productos.Control
 
         private void agregar(object sender, EventArgs e)
         {
-            producto.agregar( new Producto(
-                0,
-                vistaPro.txtNombre.Text,
-                Int32.Parse(vistaPro.txtCodigo.Text),
-                vistaPro.cbStock.Text == "Activo",
-                vistaPro.dtFecha.Text,
-                vistaPro.txtDescripcion.Text,
-                Int32.Parse(vistaPro.cbCategoria.Text),
-                vistaPro.cbEstado.Text == "Activo"
-                ));
-            vistaPro.dataGridProductor.DataSource = producto.verRegistro();           
+            contenedor.Nombre = vistaPro.txtNombre.Text;
+            contenedor.Stock = vistaPro.cbEstado.SelectedItem.Equals("Activo");
+            contenedor.FechaVencimiento = vistaPro.dtFecha.Text;
+            contenedor.Descripcion = vistaPro.txtDescripcion.Text;
+            contenedor.Categoria = Int32.Parse(vistaPro.cbCategoria.Text);
+            contenedor.Estado = vistaPro.cbEstado.SelectedItem.Equals("Activo");
+            producto.agregar(contenedor);
+            System.Windows.Forms.MessageBox.Show("Producto Agregado.");
+            vistaPro.dataGridProducto.DataSource = producto.verRegistro();
         }
 
         private void modificar(object sender, EventArgs e)
         {
-        
-            
+            contenedor.Id = Int32.Parse(vistaPro.txtID.Text);
+            contenedor.Nombre = vistaPro.txtNombre.Text;
+            contenedor.Stock = vistaPro.cbEstado.SelectedItem.Equals("Activo");
+            contenedor.FechaVencimiento = vistaPro.dtFecha.Text;
+            contenedor.Descripcion = vistaPro.txtDescripcion.Text;
+            contenedor.Categoria = Int32.Parse(vistaPro.cbCategoria.Text);
+            contenedor.Estado = vistaPro.cbEstado.SelectedItem.Equals("Activo");
+            producto.modificar(contenedor);
+            System.Windows.Forms.MessageBox.Show("Producto Modificado.");
+            vistaPro.dataGridProducto.DataSource = producto.verRegistro();
         }
 
         private void eliminar(object sender, EventArgs e)
         {
-
+            contenedor.Id = Int32.Parse(vistaPro.txtID.Text);
+            producto.eliminar(contenedor);
+            System.Windows.Forms.MessageBox.Show("Producto Eliminado.");
+            vistaPro.dataGridProducto.DataSource = producto.verRegistro();
         }
 
         private void buscar(object sender, EventArgs e)
         {
-        
-
+            contenedor.Nombre = vistaPro.txtBuscarProducto.Text;
+            vistaPro.dataGridProducto.DataSource = producto.filtrar(vistaPro.txtBuscarProducto.Text);
+            System.Windows.Forms.MessageBox.Show("Producto Encontrado.");
         }
 
         //Metodos de apoyo
